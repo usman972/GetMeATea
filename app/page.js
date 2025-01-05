1b222934616b11fa8dd3c643855781530fcf5102
usman972 <usman083922@gmail.com>
2025-01-05 20:56:33 +0500

    created the ui of login page

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <div className="flex justify-center flex-col text-white text-lg  gap-4 font-bold h-[44vh] items-center">
         <div className="font-bold flex gap-2 justify-center items-center text-4xl">
-          Buy Me A Chai{" "}
+          Buy Me A Tea!{" "}
           <span>
             {" "}
             <img src="/tea.gif" width={66} alt="" />
@@ -32,10 +32,10 @@ export default function Home() {
       </div>
       <div className="bg-white h-1 opacity-10">sdf</div>
 
-      <div className="text-white container mx-auto py-32">
-        <h1 className="text- font-bold my-2 text-center">
-          Your friends can buy you a chai.
-        </h1>
+      <div className="text-white container mx-auto pb-32 pt-14">
+        <h2 className="text-3xl font-bold mb-14 text-center">
+          Your friends can buy you a tea.
+        </h2>
         <div className="flex gap-5 justify-around ">
           <div className="item space-y-3 flex flex-col justify-center items-center">
             <img
@@ -78,48 +78,18 @@ export default function Home() {
 
       <div className="bg-white h-1 opacity-10"></div>
 
-      <div className="text-white container mx-auto py-32">
-        <h1 className="text- font-bold my-2 text-center">
+      <div className="text-white container mx-auto pb-32 pt-14 flex flex-col items-center justify-center">
+        <h2 className="text-3xl font-bold mb-14 text-center">
           Learn more about us.
-        </h1>
-        <div className="flex gap-5 justify-around ">
-          <div className="item space-y-3 flex flex-col justify-center items-center">
-            <img
-              className=" bg-slate-400 text-black  rounded-full p-2"
-              src="/man.gif"
-              width={66}
-              alt=""
-            ></img>
-            <p className="font-bold"> Fund Yourself</p>
-            <p className=" text-center">
-              Your friends are available for you to help!
-            </p>
-          </div>
-          <div className="item space-y-3 flex flex-col justify-center items-center">
-            <img
-              className=" bg-slate-400 text-black  rounded-full p-2"
-              src="/coin2.gif"
-              width={66}
-              alt=""
-            ></img>
-            <p className="font-bold"> Fund Yourself</p>
-            <p className=" text-center">
-              Your friends are available for you to help!
-            </p>
-          </div>
-          <div className="item space-y-3 flex flex-col justify-center items-center">
-            <img
-              className=" bg-slate-400 text-black  rounded-full p-2"
-              src="/group2.gif"
-              width={66}
-              alt=""
-            ></img>
-            <p className="font-bold"> Friends want to help</p>
-            <p className=" text-center">
-              Your friends are available for you to help!
-            </p>
-          </div>
-        </div>
+        </h2>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/dJQn4DqzMVQ?si=KukZVeqlf4meabCH"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
       </div>
     </>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className='bg-gray-900 text-white flex justify-between px-4 h-16 items-center'>
-      <div className='logo font-bold text-lg'>GetMeaChai!</div>
-      <ul className='flex justify-between gap-4'>
+    <nav className="bg-gray-900 text-white flex  justify-between px-4 h-16 items-center">
+      <div className="logo flex justify-center items-center font-bold text-lg">
+        <img src="/tea3.gif" width={44} alt=""></img>
+        <span>GetMeATea!</span>{" "}
+      </div>
+      {/* <ul className='flex justify-between gap-4'>
         <li>Home</li>
         <li>About</li>
         <li>Contact</li>
+        <li>Login</li>
         <li></li>
-        <li></li>
-      </ul>
-      
+      </ul> */}
+      <div>
+        <Link href={"/login"}> 
+        
+      <button
+        type="button"
+        className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        Login
+      </button>
+      </Link>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
